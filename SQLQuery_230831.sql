269363b8ca7260c9732bf2d99545ee744e6f68c2
Rezwan Islam <64501583+rzn-git@users.noreply.github.com>
2023-09-04 06:29:44 +0600

    Add files via upload

---
+++ SQLQuery_230831.sql
@@ -0,0 +1,26 @@
+Create Table WareHouseEmployeeDemographics 
+
+(EmployeeID int, 
+
+FirstName varchar(50), 
+
+LastName varchar(50), 
+
+Age int, 
+
+Gender varchar(50)
+
+)
+
+
+
+
+Insert into WareHouseEmployeeDemographics VALUES
+
+(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+
+(1050, 'Roy', 'Anderson', 31, 'Male'),
+
+(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+
+(1052, 'Val', 'Johnson', 31, 'Female')
