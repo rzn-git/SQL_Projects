572335dd229fbdb34eca671668706ea668ac2357
Rezwan Islam <64501583+rzn-git@users.noreply.github.com>
2023-09-06 02:16:24 +0600

    Add files via upload

---
+++ SQL Updating_Deleting Data.sql
@@ -0,0 +1,17 @@
+--select *
+--from SQLTutorial.dbo.EmployeeDemographics
+
+--update SQLTutorial.dbo.EmployeeDemographics
+--set EmployeeID = 1012, Age = 31, Gender = 'Female'
+--where FirstName= 'Holly' and LastName = 'Flax'
+
+
+
+--delete from SQLTutorial.dbo.EmployeeDemographics
+--where EmployeeID = 1005
+
+
+select *
+from SQLTutorial.dbo.EmployeeDemographics
+where EmployeeID = 1004
+delete from SQLTutorial.dbo.EmployeeDemographics where EmployeeID = 1004
